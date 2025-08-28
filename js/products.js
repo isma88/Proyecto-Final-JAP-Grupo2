9b4b5efad34f26fcbced624570c4d90046e2af72
Valentin Silva <valentinvsilva@gmail.com>
2025-08-28 10:41:34 -0300

    responsive product incomplete

--- js/products.js
+++ js/products.js
@@ -1,43 +1,45 @@
 
-let current_products = PRODUCTS_URL+'101'+EXT_TYPE
+let current_products = PRODUCTS_URL + '101' + EXT_TYPE
 
 function productList(list) {
-    let content = '';
-    for (product of list ){
+  let content = '';
+  for (product of list) {
+    let image = product.image
+    let name = product.name
+    let currency = product.currency
+    let cost = product.cost
+    let soldCount = product.soldCount
+    let description = product.description
 
-       content += ` <div class="image"> 
-                    <img src="${product.image}" alt="">
-                  <p>${product.name}</p>
-                  <div class="price">Precio:<span class="precio">${product.currency} ${product.cost}</span></div>
-                  
-                <p2 class="vendidos">
-                      ${product.soldCount}
-                    </p2>
+    content +=
+      `<div class="col">
+          <div class="card shadow rounded h-100">
+            <img src="${image}" class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column h-100">
+              <h5 class="card-title">${name}</h5>
+              <p  class="card-text" >${description}</p>
+                <p class="mt-auto card-text text-end"><small class="text-muted ">vendidos ${soldCount}</small></p>
+            </div>
+            <div class='card-footer'>
+              <small class=''> ${currency}${cost} </small> 
+            </div>
+          </div>
+        </div>`
+  }
 
-                     <p3 class= "p3">
-                      ${product.description}
-                    </p3>
-            
-                </div>`
-
-                
-
-
-    }
-
-    document.getElementById('items').innerHTML = content
+  document.getElementById('items').innerHTML = content
 
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-   getJSONData(current_products).then(function(result){
-        if (result.status === "ok"){
-            let list = result.data.products
-            
-            
-            productList(list)                
-        }
-    });
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(current_products).then(function (result) {
+    if (result.status === "ok") {
+      let list = result.data.products
+
+
+      productList(list)
+    }
+  });
 
 
 })
