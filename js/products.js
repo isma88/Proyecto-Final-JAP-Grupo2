c7dee7d57f66d044798c0294432c0a5cbb1970a5
Valentin Silva <valentinvsilva@gmail.com>
2025-09-01 20:34:26 -0300

    filters coocked

--- js/products.js
+++ js/products.js
@@ -1,7 +1,53 @@
 
-let current_products = PRODUCTS_URL + '101' + EXT_TYPE
+const current_catID = localStorage.getItem('catID')
+let current_products = PRODUCTS_URL + current_catID + EXT_TYPE
+console.log(current_products)
+let list = []
+let filteredlist = list
+
+function compareSort(a, b) {
+  return a.cost - b.cost
+
+}
+
+
+function sortListCostAsc() {
+
+  filteredlist.sort(compareSort)
+  productList(filteredlist)
+}
+
+function sortListCostDec() {
+
+  filteredlist.sort(compareSort)
+  filteredlist.reverse()
+  productList(filteredlist)
+}
+
+
+
+
+function sortListCostRange() {
+  let min = document.getElementById('min').value
+  if (min == "") {
+    min = 0
+  }
+  console.log(min)
+  let max = document.getElementById('max').value
+  if (max == "") {
+    max = 999999999999999999999999999999
+  }
+  console.log(max)
+
+  filteredlist = list.filter(a => a.cost > min && a.cost < max)
+  productList(filteredlist)
+
+}
+
 
 function productList(list) {
+
+
   let content = '';
   for (product of list) {
     let image = product.image
@@ -34,12 +80,14 @@ function productList(list) {
 document.addEventListener("DOMContentLoaded", function (e) {
   getJSONData(current_products).then(function (result) {
     if (result.status === "ok") {
-      let list = result.data.products
-
-
+      list = result.data.products
       productList(list)
     }
   });
-
-
 })
+
+
+document.getElementById('sortListCostDec').addEventListener('click', sortListCostDec)
+document.getElementById('sortListCostAsc').addEventListener('click', sortListCostAsc)
+
+document.getElementById('rangeValue').addEventListener('click', sortListCostRange)
