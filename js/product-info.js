e429901ed29ac30dab30d620fc7ed6d872754381
flormartfer <florenciamartinez.su@gmail.com>
2025-09-11 14:38:41 -0300

    si

---
+++ js/product-info.js
@@ -0,0 +1,39 @@
+const itemId = localStorage.getItem('ItemId')
+
+document.addEventListener("DOMContentLoaded", () => {
+  getJSONData(PRODUCTS_URL + itemId + EXT_TYPE).then(result => {
+    if (result.status === "ok") {
+      const lista = result.data.products
+      const producto = lista.find(p => p.id == itemId)
+
+      if (producto) {
+        mostrarProducto(producto)
+      }
+    }
+  })
+})
+
+function mostrarProducto(producto) {
+  const carouselInner = document.querySelector(".carousel-inner")
+  const nombreEl = document.querySelector("h2.card-text")
+  const precioEl = document.querySelector("p:nth-of-type(1)")
+  const categoriaEl = document.querySelector("p:nth-of-type(2)")
+  const vendidosEl = document.querySelector("p:nth-of-type(3)")
+  const descripcionEl = document.querySelector(".col-12 p")
+
+  nombreEl.textContent = producto.name
+  precioEl.innerHTML = `<strong>Precio:</strong> ${producto.currency} ${producto.cost}`
+  categoriaEl.innerHTML = `<strong>Categoría:</strong> ${producto.category || 'Autos'}`
+  vendidosEl.innerHTML = `<strong>Cantidad de vendidos:</strong> ${producto.soldCount}`
+  descripcionEl.textContent = producto.description
+
+  carouselInner.innerHTML = ""
+  producto.images.forEach((src, i) => {
+    const item = document.createElement("div")
+    item.className = `carousel-item ${i === 0 ? "active" : ""}`
+    item.innerHTML = `<img src="${src}" class="d-block w-100">`
+    carouselInner.appendChild(item)
+  })
+
+
+}
