2af2f98b500f56019d2d874f409e18c6e6d57e8e
flormartfer <florenciamartinez.su@gmail.com>
2025-09-14 20:50:29 -0300

    product info

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/50922.json"
+const current_ItemId = localStorage.getItem('ItemId')
+let current_products_info = PRODUCTS_INFO_URL + current_ItemId + EXT_TYPE
+console.log(current_products_info)
+let list = []
  
   fetch(PRODUCT_INFO_URL)
     .then(response => response.json())
@@ -30,4 +32,42 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     })
     .catch(err => console.error(err));
-});
+//const itemId = localStorage.getItem("ItemId");
+
+//document.addEventListener("DOMContentLoaded", () => {
+  //getJSONData(PRODUCTS_URL + itemId + EXT_TYPE).then((result) => {
+   // if (result.status === "ok") {
+    //  const lista = result.data.products;
+    //  const producto = lista.find((p) => p.id == itemId);
+
+     // if (producto) {
+      //  mostrarProducto(producto);
+      //}
+    //}
+ // });
+//});
+
+//function mostrarProducto(producto) {
+  //const carouselInner = document.querySelector(".carousel-inner");
+  //const nombreEl = document.querySelector("h2.card-text");
+  //const precioEl = document.querySelector("p:nth-of-type(1)");
+  //const categoriaEl = document.querySelector("p:nth-of-type(2)");
+  //const vendidosEl = document.querySelector("p:nth-of-type(3)");
+  //const descripcionEl = document.querySelector(".col-12 p");
+
+  //nombreEl.textContent = producto.name;
+  //precioEl.innerHTML = `<strong>Precio:</strong> ${producto.currency} ${producto.cost}`;
+  //categoriaEl.innerHTML = `<strong>Categoría:</strong> ${
+    //producto.category || "Autos"
+  //}`;
+  //vendidosEl.innerHTML = `<strong>Cantidad de vendidos:</strong> ${producto.soldCount}`;
+  //descripcionEl.textContent = producto.description;
+
+  //carouselInner.innerHTML = "";
+  //producto.images.forEach((src, i) => {
+    //const item = document.createElement("div");
+    //item.className = `carousel-item ${i === 0 ? "active" : ""}`;
+    //item.innerHTML = `<img src="${src}" class="d-block w-100">`;
+    //carouselInner.appendChild(item);
+  //});
+//}
