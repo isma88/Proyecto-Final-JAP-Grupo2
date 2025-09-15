6f4abe1da7ec5fa8eea44b071bb2bceb22cfd42a
flormartfer <florenciamartinez.su@gmail.com>
2025-09-15 13:21:58 -0300

    si

--- js/product-info.js
+++ js/product-info.js
@@ -23,7 +23,7 @@ const listElements = () => {
   let productImages = list.images;
   let productsoldCount = list.soldCount;
   let productid = list.id;
-  let content = `<div class="col-md-4">
+  let content = `<div class="col-md-6 row">
           <h1 class="mb-4 text-center mb-5">${name}</h1><p class="ms-3 fw-normal precio-valor"strong>Precio:</strong> ${productCurrency} ${productCost}</p>
         <p class="ms-3 mt-5 fw-normal cat"strong>Categoría:</strong> ${productCategory}</p>
         <p class="ms-3 fw-normal cat"strong>Vendidos:</strong> ${productsoldCount}</p>
@@ -50,96 +50,3 @@ const listElements = () => {
     `;
   });
 };
-
-//document.addEventListener("DOMContentLoaded", () => {
-//  const URL = "https://japceibal.github.io/emercado-api/products/50922.json";
-
-// fetch(URL)
-//  .then(response => response.json())
-//   .then(data => {
-//   document.getElementById("product-name").textContent = data.name;
-//     document.getElementById("product-price").textContent = `${data.currency} ${data.cost}`;
-//   document.getElementById("product-category").textContent = data.category;
-//   document.getElementById("product-sold").textContent = data.soldCount;
-//      document.getElementById("product-description").textContent = data.description;
-
-//  const carouselInner = document.querySelector("#carCarousel .carousel-inner");
-//  carouselInner.innerHTML = "";
-
-//  data.images.forEach((imgSrc, index) => {
-//   const item = document.createElement("div");
-//   item.classList.add("carousel-item");
-//   if (index === 0) item.classList.add("active");
-
-//  const img = document.createElement("img");
-//  img.src = imgSrc;
-//  img.classList.add("d-block", "w-100");
-//  img.alt = `Imagen ${index + 1}`;
-
-//  item.appendChild(img);
-//  carouselInner.appendChild(item);
-// });
-//  })
-//  .catch(err => console.error(err));
-//});
-
-//document.addEventListener("DOMContentLoaded", () => {
-//getJSONData(PRODUCTS_URL + itemId + EXT_TYPE).then((result) => {
-// if (result.status === "ok") {
-//  const lista = result.data.products;
-//  const producto = lista.find((p) => p.id == itemId);
-
-// if (producto) {
-//  mostrarProducto(producto);
-//}
-//}
-// });
-//});
-
-//function mostrarProducto(producto) {
-//const carouselInner = document.querySelector(".carousel-inner");
-//const nombreEl = document.querySelector("h2.card-text");
-//const precioEl = document.querySelector("p:nth-of-type(1)");
-//const categoriaEl = document.querySelector("p:nth-of-type(2)");
-//const vendidosEl = document.querySelector("p:nth-of-type(3)");
-//const descripcionEl = document.querySelector(".col-12 p");
-
-//nombreEl.textContent = producto.name;
-//precioEl.innerHTML = `<strong>Precio:</strong> ${producto.currency} ${producto.cost}`;
-//categoriaEl.innerHTML = `<strong>Categoría:</strong> ${
-//producto.category || "Autos"
-//}`;
-//vendidosEl.innerHTML = `<strong>Cantidad de vendidos:</strong> ${producto.soldCount}`;
-//descripcionEl.textContent = producto.description;
-
-//carouselInner.innerHTML = "";
-//producto.images.forEach((src, i) => {
-//const item = document.createElement("div");
-//item.className = `carousel-item ${i === 0 ? "active" : ""}`;
-//item.innerHTML = `<img src="${src}" class="d-block w-100">`;
-//carouselInner.appendChild(item);
-//});
-//}
-// let list = [];
-// const current_catID = localStorage.getItem("catID");
-// let current_products = PRODUCTS_URL + current_catID + EXT_TYPE;
-
-// document.addEventListener("DOMContentLoaded", function (e) {
-//   getJSONData(current_products).then(function (result) {
-//     if (result.status === "ok") {
-//       list = result.data;
-//       console.log(list);
-//       listElements();
-//     }
-//   });
-// });
-
-// const listElements = () => {
-//   let name = list.catName;
-//   let productName = list.product.name;
-//   let productDescription = list.product.desription;
-//   let productCost = list.product.cost;
-//   let productCurrency = list.product.currency;
-//   console.log(name);
-//   console.log(productCost);
-// };
