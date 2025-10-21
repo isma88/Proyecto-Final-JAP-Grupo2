ace7d6b81bcc236ebc9b316317adb65a75ec6c27
flormartfer <florenciamartinez.su@gmail.com>
2025-10-21 20:12:30 -0300

    navbar actualizacion

--- js/navbar.js
+++ js/navbar.js
@@ -25,6 +25,7 @@
               <a class="nav-link fw-bold" href="sell.html">Vender</a>
             </li>
             <li class="nav-item dropdown">
+            <img id="nav-profile-pic" src="img/img_perfil.png" alt="Foto de perfil" class="rounded-circle me-2" width="30" height="30" />
               <a class="nav-link dropdown-toggle d-flex align-items-center" href="login.html" id="nickname" role="button" data-bs-toggle="dropdown" aria-expanded="false"> 
               <img id="nav-profile-pic" src="img/img_perfil.png" alt="Foto de perfil" class="rounded-circle me-2" width="30" height="30" />
               <span id="nav-username"></span>
@@ -72,6 +73,10 @@
                     </button>
                   </li>
                 </ul>
+                     <li class="nav-item">
+              <a class="nav-link fw-bold" href="cart.html">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
+</svg>
               </li>
           </ul> 
         </div>
