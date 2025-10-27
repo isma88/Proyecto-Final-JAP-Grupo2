c09c21c4de8d284f17d8181079a2cb8af5671329
Valentin Silva <valentinvsilva@gmail.com>
2025-10-27 20:23:40 -0300

    carrito somewhat done

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+ document.addEventListener('DOMContentLoaded', function() {
   const container = document.querySelector("#container-carrito");
   const cartItems = [
     { nombre: "Computadora", img: 'img/prod40281_1.jpg',precio:'$1000'},
@@ -11,23 +11,60 @@ document.addEventListener('DOMContentLoaded', function() {
     if (items.length === 0) {
       container.innerHTML = `<p class="text-center">Tu carrito está vacío.</p>`;
       return;
-    }
-    container.innerHTML = `
-      <div class="list-group">
-        ${items.map((item) => 
-          `<div class="list-group-item d-flex align-items-center">
-             <img src="${item.img}" alt="${item.nombre}" class="me-3" style="width: 80px; height: 80px; object-fit: cover;">
-            <div>
-              <h5>${item.nombre}</h5>
-              <p>${item.precio}</p>
+     }
+     container.innerHTML = `
+    <ul class="list-group mx-5">    
+       
+        ${items.map((item) =>
+          
+       `
+         <li class="list-group-item">
+        <div class="row align-items-center w-100 justify-content-center">
+            <img src="${item.img}" alt="" class="prod-img">
+            <div class="col">
+              <div class="row text-center align-items-center">
+                <div class="col-md my-2 fw-bold">${item.nombre}</div>
+                <div class="col-md my-2 fst-italic">Precio: ${item.precio}</div>
+                <div class="col-md my-2 justify-content-center d-flex d-inline">
+
+                  <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn ">-</button>
+                    <input type="number" name="cant" min="1" max="69" step="1">
+                    <button type="button" class="btn ">+</button>
+                  </div>
+                  
+                </div>
+                 <div class="col-md my-2 fst-italic">Subtotal: $2,999.00</div>
+                <div class="col"> <div class="btn ms-3"><i class="bi bi-trash-fill"></i></div></div>
+              </div>
             </div>
           </div>
+           </li>
         `
-          )
-          .join("")}
-      </div>
-    `;
-  }
+     )
+         .join("")}
+     
+      
+   </ul> `;
+   }
 
   desplegarCarrito(cartItems);
+}); 
+
+const cant = document.querySelectorAll('[name="cant"]')
+cant.value = 1
+
+cant.forEach(element => {
+  element.value = 1
+  element.nextElementSibling.addEventListener('click', () => {
+    if(element.value > 0 && element.value < 69) {
+    element.value ++
+    }
+  })
+   element.previousElementSibling.addEventListener('click', () => {
+    if(element.value > 1 && element.value < 69) {
+    element.value --
+    }
+  })
+  
 });
