17ad16504bb718bd9250839b8b687c426f502be5
Valentin Silva <valentinvsilva@gmail.com>
2025-10-28 17:05:44 -0300

    eliminar

--- js/cart.js
+++ js/cart.js
@@ -1,70 +1,86 @@
- document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const container = document.querySelector("#container-carrito");
-  const cartItems = localStorage.getItem('cart')
+  let cartItems = localStorage.getItem('cart');
 
-  console.log(cartItems)
-
-  function desplegarCarrito(items) {
+  function desplegarCarrito() {
+    items = JSON.parse(localStorage.getItem('cart'))
     if (items.length === 0) {
       container.innerHTML = `<p class="text-center">Tu carrito está vacío.</p>`;
       return;
-     }
-     container.innerHTML = `
+    }
+    container.innerHTML = `
     <ul class="list-group mx-5">    
        
-        ${items.map((item) => 
-          
-       `
-         <li class="list-group-item">
-        <div class="row align-items-center w-100 justify-content-center">
-            <img src="${item.image}" alt="" class="prod-img">
-            <div class="col">
-              <div class="row text-center align-items-center">
-                <div class="col-md my-2 fw-bold">${item.name}</div>
-                <div class="col-md my-2 fst-italic">Precio: ${item.currency} ${(item.cost).toLocaleString('de-DE')}</div>
-                <div class="col-md my-2 justify-content-center d-flex d-inline">
+        ${items.map((item) =>
 
-                  <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn ">-</button>
-                    <input type="number" name="cant" value="${item.quantity}" min="1" max="69" step="1">
-                    <button type="button" class="btn ">+</button>
-                  </div>
-                  
-                </div>
-                 <div class="col-md my-2 fst-italic">Subtotal: ${item.currency} ${(item.quantity * item.cost).toLocaleString('de-DE')}</div>
-                <div class="col"> <div class="btn ms-3"><i class="bi bi-trash-fill"></i></div></div>
-              </div>
-            </div>
+      `
+      <li class="list-group-item">
+  <div class="row align-items-center w-100 justify-content-center">
+    <img src="${item.image}" alt="" class="prod-img">
+    <div class="col">
+      <div class="row text-center align-items-center">
+        <div class="col-md my-2 fw-bold">${item.name}</div>
+        <div class="col-md my-2 fst-italic">Precio: ${item.currency} ${(item.cost).toLocaleString('de-DE')}</div>
+        <div class="col-md my-2 justify-content-center d-flex d-inline">
+
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn ">-</button>
+            <input type="number" name="cant" value="${item.quantity}" min="1" max="69" step="1">
+            <button type="button" class="btn ">+</button>
           </div>
-           </li>
+
+        </div>
+        <div class="col-md my-2 fst-italic">Subtotal: <span id='prodSubtotal'>${item.currency} ${(item.quantity *
+            item.cost).toLocaleString('de-DE')}</span></div>
+        <div class="col">
+          <div class="btn ms-3 eliminar"><i id='${item.id}' class="bi bi-trash-fill"></i></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</li>
         `
-        
-     )
-         .join("")}
+
+    )
+        .join("")}
      
       
    </ul> `;
-   }
+    eliminar();
+  }
 
-  desplegarCarrito(JSON.parse(cartItems));
-}); 
+  desplegarCarrito();
+  eliminar();
 
-document.addEventListener('DOMContentLoaded', () =>{ 
   const cant = document.querySelectorAll('[name="cant"]')
-cant.value = 1
+  cant.value = 1
 
-cant.forEach(element => {
-  element.value = 1
-  element.nextElementSibling.addEventListener('click', () => {
-    if(element.value > 0 && element.value < 69) {
-    element.value ++
-    }
-  })
-   element.previousElementSibling.addEventListener('click', () => {
-    if(element.value > 1 && element.value < 69) {
-    element.value --
-    }
-  })
-  
-});
+  cant.forEach(element => {
+    element.value = 1
+    element.nextElementSibling.addEventListener('click', () => {
+      if (element.value > 0 && element.value < 69) {
+        element.value++
+      }
+    })
+    element.previousElementSibling.addEventListener('click', () => {
+      if (element.value > 1 && element.value < 69) {
+        element.value--
+      }
+    })
+
+  });
+  function eliminar() {
+    document.querySelectorAll('.eliminar').forEach((e) => {
+      e.addEventListener('click', (e) => {
+        e.stopImmediatePropagation()
+        //  console.log(e.currentTarget.children[0].id)
+        id = e.currentTarget.children[0].id
+
+        removeProductdeCart(id)
+        desplegarCarrito()
+      })
+    })
+  }
+
+
 })
