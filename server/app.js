4415be223e76da84c11f7457a280ab470aab62f8
flormartfer <florenciamartinez.su@gmail.com>
2025-11-23 12:06:02 -0300

    token

--- server/app.js
+++ server/app.js
@@ -18,31 +18,36 @@ app.post("/login",(req,res) => {
       res.status(401).json({message:"usuario y/o contraseña incorrecto"});
    }
 });
-app.use("/cat",(req, res, next) => {
-   try{
-      const decod
-   }
+
+app.use('/api', (req, res, next) => {
+   try {
+   const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
+   console.log(decoded)
+   next();
+} catch(err) { 
+   res.status(401).json ({message:"Usuario denegado"});
 }
-)
+});
+
 //get categories
 const CATEGORIES_URL = require("./emercado-api-main/cats/cat")
-app.get("/cat", (req, res) => { 
+app.get("/api/cat", (req, res) => { 
       res.json(CATEGORIES_URL)
 })
 
 //get productos
-app.get("/cats_products:id", (req, res) => {
+app.get("/api/cats_products:id", (req, res) => {
    let link = require("./emercado-api-main/cats_products/" + req.params.id) 
       res.json(link)
 })
 
 //get products info
-app.get("/products:id", (req, res) => {
+app.get("/api/products:id", (req, res) => {
    let link =  require("./emercado-api-main/products/" + req.params.id)
       res.json(link)
 })
 
-app.get("/products_comments/:id", (req, res) => {
+app.get("/api/products_comments/:id", (req, res) => {
    let link =  require("./emercado-api-main/products_comments/" + req.params.id)
       res.json(link)                                                                                         
 })
